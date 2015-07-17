93e22d650062869daaa2f1c33d09f3cebd5acdab
Max Jacobson <max@hardscrabble.net>
2015-07-17 03:43:32 -0400

    Implement a gem!

--- lib/rubocop_prefer_binstubs.rb
+++ lib/rubocop_prefer_binstubs.rb
@@ -1,5 +1,17 @@
 require "rubocop_prefer_binstubs/version"
+require "rubocop"
 
-module RubocopPreferBinstubs
-  # Your code goes here...
+module RuboCop
+  module Cop
+    module Lint
+      class Binstubs < Cop
+        def on_str(node)
+          return unless node.to_a.join.include?("bundle exec")
+          msg = "You reference bundle exec... use a binstub!"
+          add_offense(node, :expression, msg)
+        end
+      end
+    end
+  end
 end
+
